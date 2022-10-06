50967f3b44e2836da59c8071a670f5807d04a653
student-a1725250 <a1725250@student.adelaide.edu.au>
2022-10-06 14:25:02 +1030

    class basis written

--- farm.h
+++ farm.h
@@ -8,9 +8,22 @@
 using namespace std;
 
 /* this is the parent class that will hold the dyamically allocated arrays of the animals and crops
-
+also contains the control for the user to win the game. 
+the time/amount of days that have passed will be stored in this class.
+it will contain functions to print the owned animals/crops of the user
 */
 class Farm {
-    private
+    private:
+        int time;
+        int bank_account;
+        Animals* animal_array;
+        Produce* produce_array;
+    public:
+        bool win();
+        void print_crops();
+        void print_animals();
 
-}
+
+};
+
+#endif
