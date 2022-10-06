1ddcbbd1a4f953c2e9ce0b5409c87f0ee65f151c
student-a1725250 <a1725250@student.adelaide.edu.au>
2022-10-06 13:56:52 +1030

    initial test of farm.h upload

---
+++ farm.h
@@ -0,0 +1,16 @@
+#ifndef FARM_H
+#define FARM_H
+
+#include <string>
+#include <iostream>
+
+
+using namespace std;
+
+/* this is the parent class that will hold the dyamically allocated arrays of the animals and crops
+
+*/
+class Farm {
+    private
+
+}
