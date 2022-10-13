85455eac4a8d4141c0d1adbc2367ce5fff8d2250
student-a1725250 <a1725250@student.adelaide.edu.au>
2022-10-13 14:02:06 +1030

    nothing

---
+++ animals.h
@@ -0,0 +1,17 @@
+#ifndef ANIMALS_H
+#define ANIMALS_H
+
+#include "farmer.h"
+#include "farm.h"
+#include "farm_goods.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Animals : public Farm_goods {
+
+
+};
+
+#endif
