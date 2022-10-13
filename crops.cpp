5ebfb91121a28de231c8756ce4fbec83f9e9fac5
Jack  Locke <jacklocke@student-10-201-16-008.wireless.adelaide.edu.au>
2022-10-13 14:28:10 +1030

    rough start on animal and crop classes

---
+++ crops.cpp
@@ -0,0 +1,27 @@
+#include "crops.h"
+#include <string>
+#include <iostream>
+#include "wheat.h"
+#include "corn.h"
+#include "soybean.h"
+
+using namespace std;
+
+Crops::Crops() {
+        int wheat_hydration = 100;
+        int soybean_hydration = 100;
+        int corn_hydration = 100;
+        int wheat_price = 50;
+        int soybean_price = 150;
+        int corn_price = 100;
+}
+
+void water() {
+
+
+};
+
+void die()  {
+
+    ;
+}
