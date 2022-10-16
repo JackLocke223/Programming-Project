ae991c92fa86e2e71c3ea9983ed529e897348dad
student-a1725250 <a1725250@student.adelaide.edu.au>
2022-10-16 21:26:39 +1030

    i didnt change anything

--- animals.cpp
+++ animals.cpp
@@ -17,67 +17,25 @@ Animals::Animals() {
 }
 
 void Animals::feed() {
-
-    //if (chosen feed_cow) involves do_task
-    Animals::cow_hunger;
-    if(cow_hunger>=100);{
-        cout << "Can not feed due to cow being full." << endl;
-    };
-    if(cow_hunger<100){
-        cow_hunger=cow_hunger+10;
-        cout<< "The cows hunger is at" <<Animals::cow_hunger << endl;
-    }
-
-
-        //if (chosen feed_sheep) involves do_task
+    //if (chosen cow) involves do_task
+    Animals::cow_hunger= 100;
+        //if (chosen sheep) involves do_task
     Animals::sheep_hunger= 100;
-        if(sheep_hunger==100){
-        cout << "Can not feed due to sheep being full." << endl;
-    };
-        if(sheep_hunger<100){
-        sheep_hunger=sheep_hunger+10;
-        cout<< "The sheeps hunger is at" <<Animals::sheep_hunger << endl;
-
-    }
-
-
-        //if (chosen feed_pig) involves do_task
+        //if (chosen pig) involves do_task
     Animals::pig_hunger= 100;
-        if(pig_hunger==100){
-        cout << "Can not feed due to pig being full." << endl;
-    };
-        if(pig_hunger<100){
-        pig_hunger=pig_hunger+10;
-        cout<< "The pigs hunger is at" <<Animals::pig_hunger << endl;
-
-    }
 
 
 };
 void Animals::die()  {
-        if(cow_hunger = 0) {
+        // if hunger goes to 0 of cow
         Animals::cow_chance_of_death = 100; // and dies
-        }
-        if(cow_chance_of_death = 100){
-            cow_count=cow_count-1;
-            cout << "Cow has died!!!!." << endl;
-        }
-
-        if(sheep_hunger = 0) {
+        Cows::=cow_count-1;
+        // if hunger goes to 0 of sheep
         Animals:: sheep_chance_of_death = 100;// and dies
-        }
-        if(sheep_chance_of_death = 100){
-            sheep_count=sheep_count-1;
-            cout << "Sheep has died!!!!." << endl;
-        }
-
-        if(pig_hunger = 0) {
+        Farm::sheep_count=sheep_count-1;
+        // if hunger goes to 0 of pig
         Animals:: pig_chance_of_death = 100;// and dies
-        }
-        if(pig_chance_of_death = 100){
-            pig_count=pig_count-1;
-            cout << "Pig has died!!!!." << endl;
-        }
+        Farm::pig_count=pig_count-1;
     ;
 }
 
