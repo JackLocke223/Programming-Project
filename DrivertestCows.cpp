67a68e7ab5de9eecc04bada57f346123d64b0441
student-a1725250 <a1725250@student.adelaide.edu.au>
2022-10-17 17:50:41 +1030

    driver test

---
+++ DrivertestCows.cpp
@@ -0,0 +1,35 @@
+#include "farm.h"
+#include "farm_goods.h"
+#include "animals.h"
+#include "cows.h"
+//#include "farmer.h"
+
+
+int main (){
+    //cout << "1" << endl;
+    Farm farm;
+   // cout << "2" << endl;
+    //Farmer farmer1;
+    Cows first_cow;
+    Cows second;
+    Cows third;
+    //cout << "3" << endl;
+    // Cows *c = new Cows();
+    cout << first_cow.get_type() << endl;
+
+    farm.buy_goods(first_cow);
+    farm.buy_goods(second);
+    farm.buy_goods(third);
+    //cout << "4" << endl;
+    for (int i = 0; i < 3; i++) {
+    cout << "age of " << i << " cow: " << farm.Farm::cow_array_new[i].get_age() << "i: " << i << endl;
+    }
+    // //farmer1.sleep();
+    // for (int i = 0; i < 3; i++) {
+    // cout << "age of " << i << " cow: " << farm.Farm::cow_array_new[i].get_age() << "i: " << i << endl;
+    // }
+
+    //Cows second_cow;
+    //second_cow.Cows::buy_goods();
+
+}
